7d5e8ff61c7cd776dec5010c245d7714353f2b81
DESKTOP-SCMGQS9\Antonio <pedro9122002@gmail.com>
2025-11-20 17:48:18 +0100

    findAll created with filters and validation environment variables

--- README.md
+++ README.md
@@ -19,10 +19,20 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
-5. Reconstruir la BD con la semilla (solo en desarrollo)
+5. Clonar el archivo __.env.template__ y renombrar la copia a __.env__
+
+6. Llenar las variables de entorno definidas en el __.env__
+
+7. Ejecutar la aplicación de desarrollo:
+```
+npm run start:dev
+```
+
+8. Reconstruir la BD con la semilla (solo en desarrollo)
 ```
 http://localhost:3000/api/v2/seed
 ```
+
 
 ## Stack usado
 * MondoDB
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,30 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    //configuracion variables de entorno
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema
+    }),
+
+    //configuracion contenido estatico
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public')
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    //configuracion mongoDB
+    MongooseModule.forRoot(process.env.MONGODB!, {
+      dbName: 'pokemonsdb'
+    }),
 
     PokemonModule,
-
     CommonModule,
-
     SeedModule
   ]
 })
